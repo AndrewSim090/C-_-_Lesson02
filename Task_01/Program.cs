f786b12025cb906918c03be791f9dc483025f5ac
Andrey_Sim <demenyuk906@gmail.com>
2023-04-12 17:26:13 +0500

    Task01

---
+++ Task_01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число : ");
+string UserNamber1 = Console.ReadLine() ?? "";
+int namber_a = int.Parse(UserNamber1);
+
+int a1 = namber_a / 10;
+int a2 = a1 % 10;
+
+Console.WriteLine(a2);
